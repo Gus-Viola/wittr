6202d7014541c9a60f6acbe2b6917d303f8d9416
Gus Viola <gus.viola@pobox.com>
2018-08-22 10:31:56 -0300

    Register service worker

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -8,6 +8,24 @@ export default function IndexController(container) {
   this._toastsView = new ToastsView(this._container);
   this._lostConnectionToast = null;
   this._openSocket();
+  //I added the line below
+  this._registerServiceWorker();
+}
+
+//added the function below
+IndexController.prototype._registerServiceWorker = function() {
+  //from https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerContainer/register
+  if ('serviceWorker' in navigator) {
+    // Register a service worker hosted at the root of the
+    // site using the default scope.
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('Service worker registration succeeded:', registration);
+    }, /*catch*/ function(error) {
+      console.log('Service worker registration failed:', error);
+    });
+  } else {
+    console.log('Service workers are not supported.');
+  }
 }
 
 // open a connection to the server for live updates
