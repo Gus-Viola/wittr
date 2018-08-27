9e85ea2323e211d425db8cfc834f8a238864e5ec
Gus Viola <gus.viola@pobox.com>
2018-08-27 11:40:58 -0300

    Cache updates

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,12 @@
+const staticCacheName = 'wittr-static-v2';
+
+
 self.addEventListener('install', function(event) {
   event.waitUntil(
     // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
     // Ensure at least $primary-color changes
     // TODO: change cache name to 'wittr-static-v2'
-    caches.open('wittr-static-v1').then(function(cache) {
+    caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -13,12 +16,25 @@ self.addEventListener('install', function(event) {
         'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
       ]);
     })
+
   );
 });
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    // TODO: remove the old cache
+    caches.keys().then(function(cacheNames) {
+      console.log(cacheNames);
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('wittr-') &&
+            cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          console.log(cacheName);
+          return caches.delete(cacheName);
+        })
+      );
+
+    })
   );
 });
 
